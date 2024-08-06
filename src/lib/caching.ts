bbba3a7dc0cf258cbe214b741bc69f4e553386d4
GatsinziMeek <hackergastinzi@gmail.com>
2024-08-06 17:48:00 +0200

    new features added

---
+++ src/lib/caching.ts
@@ -0,0 +1,13 @@
+import { unstable_cache as nextcache } from "next/cache";
+import { cache as reactcache } from "react";
+
+type Callback = ([...args]: any) => Promise<any>
+
+export const cache = <TF extends Callback>(Cb: TF, keyparts:string[], options: {revalidate?: number | false; tag?: string[]} = {}) =>{
+    return nextcache(reactcache(Cb), keyparts, options)
+}
+
+
+// function cache<T extends Callback>(cb: T, keyparts, options){
+
+// }
