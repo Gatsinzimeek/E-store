b25a21a2994d9bf0fcf90a6bde001102a9954f02
GatsinziMeek <hackergastinzi@gmail.com>
2024-08-22 17:18:09 +0200

    new features

--- src/app/(customerFacing)/products/[id]/purchase/_Component/CheckOutForm.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/_Component/CheckOutForm.tsx
@@ -1,15 +1,25 @@
-
+"use client"
+import { Elements, PaymentElement, useElements, useStripe } from "@stripe/react-stripe-js"
+import { loadStripe } from "@stripe/stripe-js"
 type CheckOutFormProps = {
     product: {},
     clientSecret: string
 }
+const stripePromise = loadStripe(process.env.NEXT_Stripe_Public_Key as string)
 
 const CheckOutForm = ({product, clientSecret}: CheckOutFormProps ) => {
   return (
-    <div>
-      
-    </div>
+    <Elements stripe={stripePromise} options={{clientSecret}}>
+        <Form />
+    </Elements>
   )
+}
+
+const Form = () => {
+  const stripe = useStripe();
+  const Element = useElements();
+
+  return <PaymentElement />
 }
 
 export default CheckOutForm
